dc1a2c98ccd04e44ec23459187b536ca6e7f88bb
HappyBG <enrique.baldeos@pucp.pe>
2020-12-28 08:34:13 -0500

    Update custom_order.java

--- custom_order.java
+++ custom_order.java
@@ -4,13 +4,20 @@ import java.util.Scanner;  // Needed for the Scanner class to read input
 
 public class custom_order {
 
-    // STEP 1 PRINTING HELLO WORLD TO CONSOLE
+    int totalCost;
+    String addOnList;
+    static Void addItem(String item, int cost) {
+      totalCost+=cost;
+      addOnList+=item;
+    }
+     // STEP 1 PRINTING HELLO WORLD TO CONSOLE
     public static void main(String[] args) {
+
         
       //System.out.println("Hello World!"); // print Hello World to console
         
     // TEST CODE
-    
+
     // STEP 2 CREATE A SCANNER OBEJCT, DECLARE VARAIBLES, & PRINT STATEMENTS
 	  Scanner keyboard = new Scanner (System.in); 
 
@@ -25,7 +32,6 @@ public class custom_order {
     final double TAX_RATE = .08;  // Sales tax rate
     double tax; // Amount of tax
 
-	 
     // Introduce shop and prompt user to input first name
     System.out.println("Welcome to Java's Cake & Cupcake Shop!");
     System.out.println("We make custom cakes with our secret cake batter!");
@@ -68,24 +74,32 @@ public class custom_order {
     // TEST CODE
       
     // STEP 6 PROMPT USER TO CHOOSE FROSTING
-    System.out.println("What type of FROSTING do you want? ");
-    System.out.println("Vanilla, Chocolate, Strawberry or Coco");
+    System.out.println("Do you want Frosting? We have Vanilla, Chocolate, Strawberry and Coco ");
     frostingType = keyboard.nextLine();
+    if(frostingType != "no"){
+      addItem(frostingType, 2);
+      addOnList+=", ";
+    }
       
     //TEST CODE
       
     // STEP 7 PROMPT USER TO CHOOSE FILLING
-    System.out.println("What type of FILLING do you want? ");
-    System.out.println("Mocha, Mint, Lemon, Caramel or Raspberry");
+    System.out.println("Do you want Filling? We have Mocha, Mint, Lemon, Caramel and Raspberry");
 	  fillingType = keyboard.nextLine();
-	  
+	  if(fillingType != "no"){
+      addItem(fillingType, 2);
+      addOnList+=", ";
+    }
       
     // TEST CODE
       
     // STEP 8 PROMPT USER TO CHOOSE TOPPINGS
-    System.out.println("What type of TOPPINGS do you want? ");
-    System.out.println("Sprinkles, Cinnamon, Cocoa, Nuts");
+    System.out.println("Do you want Topping? We have Sprinkles, Cinnamon, Cocoa, and Nuts ");
     toppings = keyboard.nextLine();
+    if(toppings != "no"){
+      addItem(toppings, 2);
+      addOnList+=", ";
+    }
       
       
     // TEST CODE
@@ -107,5 +121,4 @@ public class custom_order {
     tax = cost * TAX_RATE;
     System.out.printf("The tax is: $%.2f\n", tax);
     System.out.printf("The total due is: $%.2f\n", (tax + cost));
-    }   
 }
